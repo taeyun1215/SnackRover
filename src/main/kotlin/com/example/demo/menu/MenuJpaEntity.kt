00009a69eb6d7b32c63ebfabc4837efd07694de9
taeyun1215 <taeyun1215@naver.com>
2024-02-20 22:13:56 +0900

    feat : create Entity (FoodTruck, Menu, Review)

---
+++ src/main/kotlin/com/example/demo/menu/MenuJpaEntity.kt
@@ -0,0 +1,20 @@
+package com.example.demo.menu
+
+import com.example.demo.foodTruck.FoodTruckJpaEntity
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "menus")
+data class MenuJpaEntity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null, // 메뉴의 고유 식별자입니다.
+
+    var name: String, // 메뉴의 이름입니다.
+    var description: String, // 메뉴에 대한 설명입니다.
+    var price: Float, // 메뉴의 가격입니다.
+
+    @ManyToOne
+    @JoinColumn(name = "food_truck_id")
+    var foodTruckJpaEntity: FoodTruckJpaEntity // 이 메뉴를 제공하는 푸드트럭에 대한 참조입니다.
+)
---
+++ src/main/kotlin/com/example/demo/review/ReviewJpaEntity.kt
@@ -0,0 +1,27 @@
+package com.example.demo.review
+
+import com.example.demo.foodTruck.FoodTruckJpaEntity
+import com.example.demo.user.domain.User
+import com.example.demo.user.infrastructure.UserJpaEntity
+import jakarta.persistence.*
+import java.time.LocalDateTime
+
+@Entity
+@Table(name = "reviews")
+data class ReviewJpaEntity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null, // 리뷰의 고유 식별자입니다.
+
+    var rating: Int, // 리뷰에 부여된 별점입니다.
+    var comment: String, // 리뷰 코멘트입니다.
+    var reviewDate: LocalDateTime, // 리뷰가 작성된 날짜와 시간입니다.
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    var userJpaEntity: UserJpaEntity, // 리뷰를 작성한 사용자에 대한 참조입니다.
+
+    @ManyToOne
+    @JoinColumn(name = "food_truck_id")
+    var foodTruckJpaEntity: FoodTruckJpaEntity // 리뷰 대상 푸드트럭에 대한 참조입니다.
+)
