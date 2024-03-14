a3d8107ab7ede30ba9dcf88c95a663851d206341
taeyun1215 <taeyun1215@naver.com>
2024-03-14 16:45:32 +0900

    refactor : delete unnecessary code

--- sr-core-module/src/main/kotlin/com/example/demo/SnackRoverApplication.kt
+++ sr-core-module/src/main/kotlin/com/example/demo/SnackRoverApplication.kt
@@ -2,7 +2,6 @@ package com.example.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.domain.EntityScan
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
