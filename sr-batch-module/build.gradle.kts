69f24a7c9b48c8a3a7a1663431d81b33661f7a00
taeyun1215 <taeyun1215@naver.com>
2024-03-14 17:30:04 +0900

    feat : batch module

---
+++ sr-batch-module/build.gradle.kts
@@ -0,0 +1,8 @@
+
+dependencies {
+    implementation(project(":sr-storage-module"))
+
+    implementation("org.springframework.boot:spring-boot-starter-batch")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-web")
+}
